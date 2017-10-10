9ff3dee5ecd48d6e89e6d0f55cdf29683b7b9031
CallMeInk <907022926@qq.com>
2017-10-10 19:34:39 +0800

    Update MainActivity.java
    add 11

--- app/src/main/java/com/example/yukai/listview/MainActivity.java
+++ app/src/main/java/com/example/yukai/listview/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends Activity {
 //        mListView.setSelection(6);
         Log.e(TAG, "thread info: " + Thread.currentThread().getName());
         
-//        mListView.smoothScrollBy(60, 200);3
+//        mListView.smoothScrollBy(60, 200);11
 
 //        mListView.smoothScrollByOffset(60);
 
