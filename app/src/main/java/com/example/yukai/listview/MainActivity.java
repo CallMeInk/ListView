fc2d003498b95b4555c0c6f2f24eb40fd4910099
yukai <907022926@qq.com>
2017-10-10 19:31:10 +0800

    add 3

--- app/src/main/java/com/example/yukai/listview/MainActivity.java
+++ app/src/main/java/com/example/yukai/listview/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends Activity {
 //        mListView.setSelection(6);
         Log.e(TAG, "thread info: " + Thread.currentThread().getName());
         
-//        mListView.smoothScrollBy(60, 200);2
+//        mListView.smoothScrollBy(60, 200);3
 
 //        mListView.smoothScrollByOffset(60);
 
